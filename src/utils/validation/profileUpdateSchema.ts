9c67a3689eaa68b9908ede718c9a42e647dbeea1
Stephen <79639335+stephen-gift@users.noreply.github.com>
2024-07-23 21:30:20 +0100

    Yup validation schemas

---
+++ src/utils/validation/profileUpdateSchema.ts
@@ -0,0 +1,7 @@
+import * as Yup from "yup";
+
+export const ProfileUpdateSchema = Yup.object({
+  firstName: Yup.string().required("First name is required"),
+  lastName: Yup.string().required("Last name is required"),
+  email: Yup.string().email("Invalid email address").optional(),
+});
